5cd4aeb1943edefe8fd5bdcaae3528f1910cff11
Josh Mize <jgmize@gmail.com>
2012-11-05 01:12:53 -0600

    replaced print statements with logging, including logging of URL for uploaded files

--- gdrive/gdfiles/management/commands/upload_csv.py
+++ gdrive/gdfiles/management/commands/upload_csv.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from apiclient.discovery import build
@@ -5,6 +7,10 @@ from apiclient.http import MediaFileUpload
 import httplib2
 
 from gdoauth2.models import DriveCredential
+
+
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 class Command(BaseCommand):
@@ -15,10 +21,11 @@ class Command(BaseCommand):
         service = build('drive', 'v2', http=http)
         mime_type = 'text/csv'
         for filename in args:
-            print 'uploading', filename
+            logging.info('uploading %s' % filename)
             media_body = MediaFileUpload(
                 filename, mimetype=mime_type, resumable=True)
             upload = service.files().insert(
                 body=dict(title=filename, mimeType=mime_type),
                 media_body=media_body, convert=True).execute()
-            print 'File ID: %s' % upload['id']
+            logging.info('https://docs.google.com/spreadsheet/ccc?key=%s' %
+                         upload['id'])
